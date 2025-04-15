ebab194fe4d8d2e8ad74c2e416ade2a82365400a
苏青安 <992182040@qq.com>
2025-04-15 12:46:48 +0800

    增加外部数据源入参

--- src/AddressParser.php
+++ src/AddressParser.php
@@ -12,10 +12,19 @@ class AddressParser
      * 解析地址
      * @param mixed $string 地址信息
      * @param bool $user 信息中是否包含用户信息
+     * @param string $unknownValue 未匹配到数据时填充内容
+     * @param array $level1Data 一级行政区（省）数据（不传递则使用默认数据源）
+     * @param array $level2Data 二级行政区（市）数据（不传递则使用默认数据源）
+     * @param array $level3Data 三级行政区（区/县）数据（不传递则使用默认数据源）
      * 
      * @return array {name: string, mobile: string, idn: string, postcode: string, province: string, city: string, region: string, street: string} 
+     *
+     * 说明：
+     * - 省市区数据源来自 \Hejunjie\ChinaDivision\Division::getCityLevels()
+     * - 格式为：['id'=>['name'=>'城市名称','pid'=>'上级ID，省级为0']]
+     * - 详见：https://github.com/zxc7563598/php-china-division
      */
-    public static function parse(string $string, bool $user = true)
+    public static function parse(string $string, bool $user = true, $unknownValue = '未知', array $level1Data = [], array $level2Data = [], array $level3Data = [])
     {
         // 默认数据
         $data = [
@@ -42,7 +51,7 @@ class AddressParser
             $addr = $string;
         }
         // 解析地址
-        $address = RegionMatcher::parseAddress($addr);
+        $address = RegionMatcher::parseAddress($addr, $unknownValue, $level1Data, $level2Data, $level3Data);
         // 更新地址信息
         $data['province'] = $address['province'];
         $data['city'] = $address['city'];
